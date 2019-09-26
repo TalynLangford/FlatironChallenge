3e6f5b5f046f41c26f5d99de2c0c988f125d2a0e
TalynLangford <talyn.langford@gmail.com>
2019-09-25 20:06:30 -0700

    quack 2, the quackening

--- quack.js
+++ quack.js
@@ -10,25 +10,29 @@ rl.on('line', (line) => lines.push(line));
 rl.on('close', (line) => compute(lines));
 
 function compute(lines){
-   let answer = ''
-    let mainIndex = 0;
+   let answer = '';
+    let index = 0
+    let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
    for (let i = 0; i < lines.length; i++){
-       let index = mainIndex
-        let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    //    let index = mainIndex
         let lineArray = lines[i].split(' ')
         for (let j = 0; j < lineArray.length; j++){
             if (lineArray[j] == 'qwak'){
                 index = index + 1
             }
-            if (lineArray[j] == 'quack'){
+            else if (lineArray[j] == 'quack'){
                 index = index - 1
             }
-            console.log(lineArray, alphabet[index])
+            else if (lineArray[j] == "qwuack")
+            {
+                answer = answer + alphabet[index]
+            }
+            // console.log(lineArray, alphabet[index])
             
         }
-        answer = answer + alphabet[index]
-        mainIndex = index
+        // answer = answer + alphabet[index]
+        // mainIndex = index
         
     // answer = answer + alphabet[index]   
    }
